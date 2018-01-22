144fb2f98540721cc21893e5451b84af9dbcbf34
eistee93 <jann.schuette@ymail.com>
2018-01-22 15:30:07 +0100

    added solution, project and mvvm helper classes

---
+++ PriceTag/MVVM/INotifyPropertyChangedBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace PriceTag.MVVM
+{
+   class INotifyPropertyChangedBase
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      public void NotifyPropertyChanged([CallerMemberName] string memberName = null)
+      {
+         if (PropertyChanged != null)
+         {
+            PropertyChanged(this, new PropertyChangedEventArgs(memberName));
+         }
+      }
+
+      public void NotifyPropertyChanged<T>(ref T fieldValue, T value, [CallerMemberName] string memberName = null)
+      {
+         fieldValue = value;
+
+         if (PropertyChanged != null)
+         {
+            PropertyChanged(this, new PropertyChangedEventArgs(memberName));
+         }
+      }
+   }
+}
